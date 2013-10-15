1f81d7c934f6e4957571d0dbf980d659282c237c
huang.xinghui <huang.x.hui@gmail.com>
2013-10-15 19:33:54 +0800

    add datepicker

--- src/ztesoft/components/numericstepper.js
+++ src/ztesoft/components/numericstepper.js
@@ -1,11 +1,30 @@
-(function($, undefined) {
+(function() {
 	var ns = ztesoft.namespace("ztesoft.components");
 
 	var NumericStepper = ns.NumericStepper = function(element, options) {
 
+		_.extend(this, NumericStepper.DEFAULTS, options);
 	};
 
-	_.extend(NumericStepper.prototype, Backbone.Events, {
+	_.extend(NumericStepper.prototype, ztesoft.event.Events);
 
-	});
-})(window.jQuery);
+	NumericStepper.prototype.value = function(value) {
+		if (arguments.length === 0) {
+			return this._value;
+		}
+
+		this._setValue(value);
+	};
+
+	NumericStepper.prototype._setValue = function(value) {
+		this.inputField.text = value;
+		this._value = value;
+		this.trigger('change');
+	};
+
+	NumericStepper.DEFAULTS = {
+		minimum: 0,
+		maximum: 10,
+		stepSize: 1
+	}
+})();
---
+++ src/ztesoft/utils/dateutil.js
@@ -0,0 +1,35 @@
+(function(){
+	var ns = ztesoft.namespace('ztesoft.utils');
+
+	var MILLIS_PER_SECOND = 1000;
+	var MILLIS_PER_MINUTE = 60 * MILLIS_PER_SECOND;
+	var MILLIS_PER_HOUR = 60 * MILLIS_PER_MINUTE;
+	var MILLIS_PER_DAY = 24 * MILLIS_PER_HOUR;
+
+	function handleShorterMonth(originalDate, newDate) {
+		var result = newDate;
+		var originalDayOfMonth = originalDate.getDate();
+		if (originalDayOfMonth > result.getDate()) 
+		{
+			result = DateUtil.addDays(newDate, -(newDate.getDate()));
+		}
+		return result;
+	}
+
+	function add(date, multiplier, num) {
+		var resultTime = date.getTime() + multiplier * num;
+		return new Date(resultTime);
+	}
+
+	var DateUtil = ns.DateUtil = {
+		'addMonths': function(date, months) {
+			var result = new Date(date.getTime());
+			result.setMonth(date.getMonth() + months);
+			result = handleShorterMonth(date, result);
+			return result;
+		},
+		'addDays': function(date, days) {
+			return add(date, MILLIS_PER_DAY, days);
+		}
+	};
+})();
